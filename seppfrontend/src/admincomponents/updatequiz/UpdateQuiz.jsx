9abbddb8f707ad8cef24a9ce5545d46400f24838
Kall <samueluche432@gmail.com>
2025-04-05 17:32:59 +0100

    Updated

--- seppfrontend/src/admincomponents/updatequiz/UpdateQuiz.jsx
+++ seppfrontend/src/admincomponents/updatequiz/UpdateQuiz.jsx
@@ -10,7 +10,7 @@ const UpdateQuiz = () => {
   useEffect(() => {
     const fetchQuizzes = async () => {
       try {
-        const response = await axios.get('/quizzes')
+        const response = await axios.get('/quizzesindex')
         setQuizzes(response.data)
       } catch (error) {
         console.error('Error fetching quizzes:', error)
@@ -43,6 +43,22 @@ const UpdateQuiz = () => {
     })
   }
 
+  const toggleVisibility = async (id, currentVisibility) => {
+    try {
+      const response = await axios.patch(`/quizzes/${id}/visibility`, {
+        is_visible: !currentVisibility,
+      })
+      setQuizzes(
+        quizzes.map((quiz) =>
+          quiz.id === id ? { ...quiz, is_visible: !currentVisibility } : quiz
+        )
+      )
+    } catch (error) {
+      console.error('Error toggling visibility:', error)
+      swal('Error', 'Failed to update quiz visibility', 'error')
+    }
+  }
+
   return (
     <div className='sepp__update-quizes'>
       {quizzes.map((quiz) => (
@@ -50,9 +66,24 @@ const UpdateQuiz = () => {
           <div className='sepp__update-quiz--detail'>
             <div className='sepp__update-quiz--name'>
               <h5 className='sepp__update-quiz--title'>{quiz.title}</h5>
+              <span
+                className={`sepp__update-quiz--status ${
+                  quiz.is_visible ? 'visible' : 'hidden'
+                }`}
+              >
+                {quiz.is_visible ? 'Visible' : 'Hidden'}
+              </span>
             </div>
           </div>
           <div className='sepp__update-quiz--action'>
+            <button
+              onClick={() => toggleVisibility(quiz.id, quiz.is_visible)}
+              className={`sepp__update-quiz--toggle ${
+                quiz.is_visible ? 'visible' : 'hidden'
+              }`}
+            >
+              {quiz.is_visible ? 'Hide' : 'Show'}
+            </button>
             <Link
               to={`/admin/edit-title/${quiz.id}`}
               className='sepp__update-quiz--edit'
@@ -86,3 +117,92 @@ const UpdateQuiz = () => {
 }
 
 export default UpdateQuiz
+
+// import React, { useEffect, useState } from 'react'
+// import { Link } from 'react-router-dom'
+// import axios from 'axios'
+// import swal from 'sweetalert'
+// import './updatequiz.scss'
+
+// const UpdateQuiz = () => {
+//   const [quizzes, setQuizzes] = useState([])
+
+//   useEffect(() => {
+//     const fetchQuizzes = async () => {
+//       try {
+//         const response = await axios.get('/quizzes')
+//         setQuizzes(response.data)
+//       } catch (error) {
+//         console.error('Error fetching quizzes:', error)
+//       }
+//     }
+
+//     fetchQuizzes()
+//   }, [])
+
+//   const handleDelete = async (id) => {
+//     swal({
+//       title: 'Are you sure?',
+//       text: 'Once deleted, you will not be able to recover this quiz!',
+//       icon: 'warning',
+//       buttons: true,
+//       dangerMode: true,
+//     }).then(async (willDelete) => {
+//       if (willDelete) {
+//         try {
+//           await axios.delete(`/quizzes/${id}`)
+//           setQuizzes(quizzes.filter((quiz) => quiz.id !== id))
+//           swal('Poof! Your quiz has been deleted!', {
+//             icon: 'success',
+//           })
+//         } catch (error) {
+//           console.error('Error deleting quiz:', error)
+//           swal('Error', 'Failed to delete quiz', 'error')
+//         }
+//       }
+//     })
+//   }
+
+//   return (
+//     <div className='sepp__update-quizes'>
+//       {quizzes.map((quiz) => (
+//         <div key={quiz.id} className='sepp__update-quiz__quiz'>
+//           <div className='sepp__update-quiz--detail'>
+//             <div className='sepp__update-quiz--name'>
+//               <h5 className='sepp__update-quiz--title'>{quiz.title}</h5>
+//             </div>
+//           </div>
+//           <div className='sepp__update-quiz--action'>
+//             <Link
+//               to={`/admin/edit-title/${quiz.id}`}
+//               className='sepp__update-quiz--edit'
+//             >
+//               Edit Title
+//             </Link>
+//             <Link
+//               to={`/admin/edit-quiz/${quiz.id}`}
+//               className='sepp__update-quiz--edit'
+//             >
+//               Edit Q & A
+//             </Link>
+//             <Link
+//               to={`/admin/display-quiz/${quiz.id}`}
+//               className='sepp__update-quiz--edit'
+//             >
+//               Display Quiz
+//             </Link>
+
+//             <button
+//               onClick={() => handleDelete(quiz.id)}
+//               className='sepp__update-quiz--delete'
+//             >
+//               Delete
+//             </button>
+//           </div>
+//         </div>
+//       ))}
+//     </div>
+//   )
+// }
+
+// export default UpdateQuiz
--- seppfrontend/src/components/profile/quizlist/QuizList.jsx
+++ seppfrontend/src/components/profile/quizlist/QuizList.jsx
@@ -10,7 +10,7 @@ const QuizList = () => {
   useEffect(() => {
     const fetchQuizzes = async () => {
       try {
-        const response = await axios.get('/quizzes')
+        const response = await axios.get('/quizzes') // Update endpoint
         setQuizzes(response.data)
       } catch (error) {
         console.error('Error fetching quizzes:', error)
@@ -47,3 +47,53 @@ const QuizList = () => {
 }
 
 export default QuizList
+
+// import React, { useEffect, useState } from 'react'
+// import { BiBook } from 'react-icons/bi'
+// import { Link } from 'react-router-dom'
+// import axios from 'axios'
+// import './quizlist.scss'
+
+// const QuizList = () => {
+//   const [quizzes, setQuizzes] = useState([])
+
+//   useEffect(() => {
+//     const fetchQuizzes = async () => {
+//       try {
+//         const response = await axios.get('/quizzes')
+//         setQuizzes(response.data)
+//       } catch (error) {
+//         console.error('Error fetching quizzes:', error)
+//       }
+//     }
+
+//     fetchQuizzes()
+//   }, [])
+
+//   return (
+//     <div className='sepp__profile-quizes'>
+//       {quizzes.map((quiz) => (
+//         <Link
+//           to={`/exam/${quiz.id}`}
+//           className='sepp__profile-quiz__quiz'
+//           key={quiz.id}
+//         >
+//           <div className='sepp__profile-quiz--detail'>
+//             <div className='sepp__profile-quiz--icon'>
+//               <BiBook />
+//             </div>
+//             <div className='sepp__profile-quiz--name'>
+//               <h5 className='sepp__profile-quiz--title'>{quiz.title}</h5>
+//               <span className='sepp__profile-quiz--duration'>
+//                 {quiz.duration} Minute(s)
+//               </span>
+//             </div>
+//           </div>
+//           <div className='sepp__profile-quiz--action'>|</div>
+//         </Link>
+//       ))}
+//     </div>
+//   )
+// }
+
+// export default QuizList
